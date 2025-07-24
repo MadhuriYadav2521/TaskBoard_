a19c4cf58c55e07221976358db94d25abc661152
Yadav Madhuri <madhuri.y.0932@gmail.com>
2025-07-24 20:33:25 +0530

    gradiently icon used

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import BellRing from "./BellRing";
 
 const Navbar = () => {
     const navigate = useNavigate()
@@ -85,8 +86,9 @@ const Navbar = () => {
                             viewBox="0 0 16 16">
                             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
                         </svg>
+                        {/* <BellRing /> */}
 
-                        <span className="absolute top-4 left-2 bg-purple-400 text-black shadow-2xl text-xs px-1.5 rounded-full">
+                        <span className="absolute top-6 left-6 bg-purple-400 text-black shadow-2xl text-xs px-1.5 rounded-full">
                             {unreadChatCount}
                         </span>
                     </div>
